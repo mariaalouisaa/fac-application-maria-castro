657f668eaac7c2a5430c3c0291e6e4fcdc7b6dba
= <maria.louisa.castro@gmail.com>
2021-11-20 16:46:55 -0600

    Refactored JavaSscript
    used ternary operator in place of if/else statement

--- index.js
+++ index.js
@@ -20,38 +20,22 @@ function play() {
   let responseText = document.querySelector("#ball-text");
 
   let randomNumber = Math.floor(Math.random() * 8);
-  console.log(randomNumber);
-  if (randomNumber === 0) {
-    responseText.innerHTML = "It is most certain";
-  } else {
-    if (randomNumber === 1) {
-      responseText.innerHTML = "That may be true";
-    } else {
-      if (randomNumber === 2) {
-        responseText.innerHTML = "Reply hazy try again";
-      } else {
-        if (randomNumber === 3) {
-          responseText.innerHTML = "Cannot predict now";
-        } else {
-          if (randomNumber === 4) {
-            responseText.innerHTML = "Do not count on it";
-          } else {
-            if (randomNumber === 5) {
-              responseText.innerHTML = "My sources say no";
-            } else {
-              if (randomNumber === 6) {
-                responseText.innerHTML = "Outlook not so good";
-              } else {
-                if (randomNumber === 7) {
-                  responseText.innerHTML = "Signs point to yes";
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+
+  randomNumber === 0
+    ? (responseText.innerHTML = "It is most certain")
+    : randomNumber === 1
+    ? (responseText.innerHTML = "That may be true")
+    : randomNumber === 2
+    ? (responseText.innerHTML = "Reply hazy try again")
+    : randomNumber === 3
+    ? (responseText.innerHTML = "Cannot predict now")
+    : randomNumber === 4
+    ? (responseText.innerHTML = "Do not count on it")
+    : randomNumber === 5
+    ? (responseText.innerHTML = "My sources say no")
+    : randomNumber === 6
+    ? (responseText.innerHTML = "Outlook not so good")
+    : (responseText.innerHTML = "Signs point to yes");
 }
 
 function question(event) {
