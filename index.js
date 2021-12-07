5eb44505cff46f610dbcce2f8e4569788a9953e6
= <maria.louisa.castro@gmail.com>
2021-12-07 12:29:47 -0600

    refactored JS, used array instead of ternary operator to eliminate repetition

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Javascript for Toggle Bar
 
-let toggleBar = document.getElementById("toggle");
+const toggleBar = document.getElementById("toggle");
 
 function nightMode(event) {
   if (this.checked) {
@@ -16,34 +16,30 @@ toggleBar.addEventListener("change", nightMode);
 
 //Functionality of Fortune Teller Feature
 
-function play() {
-  let responseText = document.querySelector("#ball-text");
+let responses = [
+  "It is most certain",
+  "That may be true",
+  "Reply hazy try again",
+  "Cannot predict now",
+  "Do not count on it",
+  "My sources say no",
+  "Outlook not so good",
+  "Signs point to yes",
+];
 
+function play() {
+  const responseText = document.querySelector("#ball-text");
   let randomNumber = Math.floor(Math.random() * 8);
 
-  randomNumber === 0
-    ? (responseText.innerHTML = "It is most certain")
-    : randomNumber === 1
-    ? (responseText.innerHTML = "That may be true")
-    : randomNumber === 2
-    ? (responseText.innerHTML = "Reply hazy try again")
-    : randomNumber === 3
-    ? (responseText.innerHTML = "Cannot predict now")
-    : randomNumber === 4
-    ? (responseText.innerHTML = "Do not count on it")
-    : randomNumber === 5
-    ? (responseText.innerHTML = "My sources say no")
-    : randomNumber === 6
-    ? (responseText.innerHTML = "Outlook not so good")
-    : (responseText.innerHTML = "Signs point to yes");
+  responseText.innerHTML = responses[randomNumber];
 }
 
 function question(event) {
   event.preventDefault();
 
-  let questionInput = document.querySelector("#input-bar");
+  const questionInput = document.querySelector("#input-bar");
 
-  let questionText = document.querySelector("#question-text");
+  const questionText = document.querySelector("#question-text");
   if (questionInput.value) {
     questionText.innerHTML = `${questionInput.value}`;
   }
@@ -52,5 +48,5 @@ function question(event) {
   play();
 }
 
-let form = document.querySelector("#search-bar");
+const form = document.querySelector("#search-bar");
 form.addEventListener("submit", question);
